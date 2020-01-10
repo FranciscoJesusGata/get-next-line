f5d20a2e74abaa94d4c794bbe69f700e54d4d7b8
Francisco jesus Gata valdes <fgata-va@c2r5s6.42madrid.com>
2020-01-10 20:53:04 +0100

    Almost perfect

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: fgata-va <fgata-va@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 18:04:00 by fgata-va          #+#    #+#             */
-/*   Updated: 2020/01/09 19:05:27 by fgata-va         ###   ########.fr       */
+/*   Updated: 2020/01/10 20:50:32 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_clean_line(char *save, char **line, int r)
+char	*ft_clean_line(char *save, char **line, int *r)
 {
 	int	i;
 	int j;
@@ -27,11 +27,6 @@ char	*ft_clean_line(char *save, char **line, int r)
 			if(save[i] == '\0')
 			{
 				j = 0;
-				if (r == 0)
-				{
-					*line = save;
-					save = NULL;
-				}
 				break;
 			}
 			i++;
@@ -41,10 +36,21 @@ char	*ft_clean_line(char *save, char **line, int r)
 			*line = ft_substr(save, 0, i);
 			tmp = ft_substr(save, i + 1, ft_strlen(save));
 			free(save);
-			save = ft_strdup(tmp);
-			free(tmp);
+			save = tmp;
+			tmp = NULL;
+			*r = 1;
 			return(save);
 		}
+		else if (*r == 0 && j == 0)
+		{
+			*line = save;
+			save = NULL;
+		}
+		else
+		{
+			*line = ft_strdup("");
+		}
+		
 	}
 	return (NULL);
 }
@@ -59,6 +65,8 @@ int     	get_next_line(int fd, char **line)
 
 	while((r = read(fd, buffer, BUFFER_SIZE)))
 	{
+		if(r == -1)
+			return(-1);
 		buffer[r] = '\0';
 		if(save[fd])
 		{
@@ -70,24 +78,20 @@ int     	get_next_line(int fd, char **line)
 		else
 			save[fd] = ft_strdup(buffer);
 		i = 0;
-		while(i < r && save[fd][i] != '\n')
+		while(i < r && buffer[i] != '\n')
 		{
-			if(save[fd][i] == '\0')
-				return(0);
 			i++;
 		}
-		if(save[fd][i] == '\n')
-		{
-			free(save[fd]);
+		if(buffer[i] == '\n')
 			break;
-		}
 	}
-	save[fd] = ft_clean_line(save[fd], line, r);
-	if(r <= 0 && !save[fd])
+	if(!save[fd] && r <= 0)
 	{
 		*line = ft_strdup("");
-		return (r);
+		return(r);
 	}
+	save[fd] = ft_clean_line(save[fd], line, &r);
+	if(r <= 0)
+		return(r);
 	return (1);
 }
-
